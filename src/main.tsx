9d3f8d3bfa6e13a57c0150bb92a8663069eff9ed
Erwin Aghajani <erwin0maleki@gmail.com>
2023-07-07 12:44:23 -0700

    adding react router and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './index.css'
 import LoginPage from './pages/auth/login'
+import App from './App'
 import RegisterPage from './pages/auth/register'
+import NotFoundPage from './pages/404'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App></App>,
+    errorElement:<NotFoundPage></NotFoundPage>
+  },
+  {
+    path:'/auth/login',
+    element:<LoginPage></LoginPage>
+  },
+  {
+    path:'/auth/register',
+    element:<RegisterPage></RegisterPage>
+  },
+  
+
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-   <LoginPage/>
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
