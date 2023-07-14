c295bf64c8d2e35e2f3601ab0da3ed57e3532887
Erwin <erwin0maleki@gmail.com>
2023-07-13 17:09:30 -0700

    added image modal

---
+++ src/components/ui/modals/addImage.tsx
@@ -0,0 +1,85 @@
+import React, { FC, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles: any) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className=" h-64 w-full flex justify-center items-center bg-white rounded-2xl"
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <div className="flex flex-col">
+          <FontAwesomeIcon icon={faPlus} size={"2x"}></FontAwesomeIcon>
+          <p className="mt-2">Drag or upload your image here</p>
+          <span className="bg-red-600 p-2 rounded-lg text-center mt-4 text-white cursor-pointer">
+            Select your image
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+interface props {
+  isOpen: boolean;
+  closeCallBack: () => void;
+}
+
+const EditModal: FC<props> = ({ isOpen, closeCallBack }) => {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10 " onClose={closeCallBack}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto ">
+            <div className="flex min-h-full items-center justify-center text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl  bg-white  text-left align-middle shadow-xl transition-all">
+                  {/* <Dialog.Title
+                    as="div"
+                    className="flex justify-center cursor-pointer"
+                  ></Dialog.Title> */}
+                  <div className="">
+                    <MyDropzone></MyDropzone>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+export default EditModal;
--- src/components/ui/write.tsx
+++ src/components/ui/write.tsx
@@ -1,27 +1,66 @@
-import { faImage,faVideo,faSmile,faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {
+  faImage,
+  faVideo,
+  faSmile,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC, useRef, useState } from "react";
+import AddImageModal from "./modals/addImage";
+import EditModal from "./modals/editUser";
 
 // interface props {
 //     color?:any
 //    }
 
-const WriteComponent:FC = ()=>{
-  const [value, changeValue] = useState('');
+const WriteComponent: FC = () => {
+  const [value, changeValue] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
-    return(
-        <div className="border-b-2 border-whiteGray  p-5  ">
-    <textarea  ref={textAreaRef} value={value} rows={4} onChange={(e)=>{changeValue(e.target.value)}} placeholder='Share your thoughts...' className='bg-secondary w-full focus:outline-none text-second-accent text-xl ' />
-    <div className="flex mt-8 justify-between">
-    <div className="flex ">
-    <FontAwesomeIcon icon={faImage} className="text-low-color-accent cursor-pointer hover:text-white"></FontAwesomeIcon>
-    <FontAwesomeIcon icon={faVideo} className="text-low-color-accent cursor-pointer ml-4 hover:text-white"></FontAwesomeIcon>
-    <FontAwesomeIcon icon={faSmile} className="text-low-color-accent cursor-pointer ml-4 hover:text-white"></FontAwesomeIcon>
+  let [isEditModalOpen, setEditModalOpen] = useState(false);
+  const closeModal = () => {
+    setEditModalOpen(false);
+  };
+  const openModal = () => {
+    setEditModalOpen(true);
+  };
+  return (
+    <div className="border-b-2 border-whiteGray  p-5  ">
+      <textarea
+        ref={textAreaRef}
+        value={value}
+        rows={4}
+        onChange={(e) => {
+          changeValue(e.target.value);
+        }}
+        placeholder="Share your thoughts..."
+        className="bg-secondary w-full focus:outline-none text-second-accent text-xl "
+      />
+      <div className="flex mt-8 justify-between">
+        <div className="flex ">
+          <FontAwesomeIcon
+            icon={faImage}
+            className="text-low-color-accent cursor-pointer hover:text-white"
+            onClick={openModal}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faVideo}
+            className="text-low-color-accent cursor-pointer ml-4 hover:text-white"
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            icon={faSmile}
+            className="text-low-color-accent cursor-pointer ml-4 hover:text-white"
+          ></FontAwesomeIcon>
+          <AddImageModal
+            isOpen={isEditModalOpen}
+            closeCallBack={closeModal}
+          ></AddImageModal>
+        </div>
+        <button className="bg-red-600 text-white px-3 py-2 rounded-full -mt-4 hover:opacity-50">
+          <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+        </button>
+      </div>
     </div>
-    <button className='bg-red-600 text-white px-3 py-2 rounded-full -mt-4 hover:opacity-50'><FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon></button>
-    </div>
-  </div>
-    )
-}
+  );
+};
 
 export default WriteComponent;
