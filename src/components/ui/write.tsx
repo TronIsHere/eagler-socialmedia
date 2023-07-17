9ab58f53f53a874df45e9de30b9e936c49d4a924
Erwin <erwin0maleki@gmail.com>
2023-07-17 16:26:58 -0700

    added emoji panel to home route

--- src/components/ui/write.tsx
+++ src/components/ui/write.tsx
@@ -5,6 +5,7 @@ import {
   faPaperPlane,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 import { FC, useRef, useState } from "react";
 import AddImageModal from "./modals/addImage";
 import EditModal from "./modals/editUser";
@@ -15,6 +16,7 @@ import EditModal from "./modals/editUser";
 
 const WriteComponent: FC = () => {
   const [value, changeValue] = useState("");
+  const [showEmoji, setShowEmoji] = useState(false);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   let [isEditModalOpen, setEditModalOpen] = useState(false);
   const closeModal = () => {
@@ -22,6 +24,9 @@ const WriteComponent: FC = () => {
   };
   const openModal = () => {
     setEditModalOpen(true);
+  };
+  const addemoji = (emojiData: EmojiClickData) => {
+    changeValue((prevstate) => `${prevstate} ${emojiData.emoji}`);
   };
   return (
     <div className="border-b-2 border-whiteGray  p-5  ">
@@ -31,6 +36,7 @@ const WriteComponent: FC = () => {
         rows={4}
         onChange={(e) => {
           changeValue(e.target.value);
+          console.log(value);
         }}
         placeholder="Share your thoughts..."
         className="bg-secondary w-full focus:outline-none text-second-accent text-xl "
@@ -49,11 +55,26 @@ const WriteComponent: FC = () => {
           <FontAwesomeIcon
             icon={faSmile}
             className="text-low-color-accent cursor-pointer ml-4 hover:text-white"
+            onClick={() => setShowEmoji(!showEmoji)}
           ></FontAwesomeIcon>
           <AddImageModal
             isOpen={isEditModalOpen}
             closeCallBack={closeModal}
           ></AddImageModal>
+          <div className="relative ml-2">
+            <div className="absolute">
+              {showEmoji ? (
+                <EmojiPicker
+                  onEmojiClick={(
+                    emojiData: EmojiClickData,
+                    event: MouseEvent
+                  ) => addemoji(emojiData)}
+                />
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
         </div>
         <button className="bg-red-600 text-white px-3 py-2 rounded-full -mt-4 hover:opacity-50">
           <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
