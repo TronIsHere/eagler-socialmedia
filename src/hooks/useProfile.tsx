d43e4cb759af2f899cc9c5bd1f60d58bd09420bc
Erwin <erwin0maleki@gmail.com>
2023-08-01 16:13:05 -0700

    removed some useless code

--- src/hooks/useProfile.tsx
+++ src/hooks/useProfile.tsx
@@ -5,14 +5,12 @@ import { useAppDispatch } from "./useRedux";
 export const useProfile = () => {
   let [isEditModalOpen, setEditModalOpen] = useState(false);
   const userData = useAppSelector(userSelector);
-  const dispatch = useAppDispatch();
   const closeModal = () => {
     setEditModalOpen(false);
   };
   const openModal = () => {
     setEditModalOpen(true);
   };
-  useEffect(() => {}, []);
   return {
     isEditModalOpen,
     closeModal,
